f981575d0983c7a7ef2ff469ab1cdfa9d3683b9a
Sami Chrafih <93194707+samiccc06@users.noreply.github.com>
2023-01-16 11:30:17 +0000

    Stap 2 1. Maak tabellen en velden (CREATE TABLE commando) in je database waarin alle informatie kan worden opgeslagen die je over je producten wilt tonen. Niet helemaal af

--- db/create.sql
+++ db/create.sql
@@ -8,7 +8,18 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  category numeric(0, 255)
+  
 );
+
+CREATE TABLE categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+);
+
+-- review ?
+-- merk ?
+-- 
 
 
 --
@@ -20,6 +31,7 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
+-- TABLE products
 insert into products (name, description, code, price) values ('GAN 13', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
 insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
@@ -27,3 +39,18 @@ insert into products (name, description, code, price) values ('Aquí llega Conde
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
 
+-- TABLE categories
+insert into categories (name) values ('2x2');
+insert into categories (name) values ('3x3');
+insert into categories (name) values ('4x4');
+insert into categories (name) values ('5x5');
+insert into categories (name) values ('6x6');
+insert into categories (name) values ('7x7');
+insert into categories (name) values ('Megaminx');
+insert into categories (name) values ('Pyraminx');
+insert into categories (name) values ('Square-1');
+insert into categories (name) values ('Skewb');
+insert into categories (name) values ('Clock');
+insert into categories (name) values ('Non-WCA');
+insert into categories (name) values ('Accessoires');
+insert into categories (name) values ('Merchandise');
