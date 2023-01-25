d9eb3889b2b3c3d6798e575e7c824dafb13ce5a7
Wille Huijing <93260754+WilleHuijing@users.noreply.github.com>
2023-01-25 10:00:11 +0000

    discriptie toegevoed

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  category numeric(0, 255)
+  categorie numeric(0, 255)
 );
 
 CREATE TABLE categories (
@@ -43,26 +43,28 @@ CREATE TABLE size_chart (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
+/* er moet nog de grootte, het gewicht, jaar waarin het is uitgebracht bij worden toegevoegd. Het beste in en tabel naast de description. */
+
 -- TABLE products
-insert into products (name, description, price, categorie) values ('YJ MGC', '', 12, 1);
-insert into products (name, description, price, categorie) values ('YJ MGC2 ELITE', '',21 , 1);
-insert into products (name, description, price, categorie) values ('X-MAN FLARE', '', 16, 1);
-insert into products (name, description, price, categorie) values ('GAN 13 Maglev', 'Gloednieuwe Kubus', 84, 2);
-insert into products (name, description, price, categorie) values ('X-MAN TORNADO V3 M', '', 25, 2);
-insert into products (name, description, price, categorie) values ('MOYU RS3 M 2020', '', 9, 2);
+insert into products (name, description, price, categorie) values ('YJ MGC 2x2', 'The YJ MGC 2x2 is a fast and flexible magnetic feel coupled with smooth, light turning. It is a very good cube for beginners.', 12, 1);
+insert into products (name, description, price, categorie) values ('YJ MGC2 ELITE', 'This cube features an innovative, first if its kind on a 2x2, center/edge magnetic mechenism that offers great control and stability.',21 , 1);
+insert into products (name, description, price, categorie) values ('X-MAN FLARE', 'It has adjustable magnets. The Flare has three different magnetic settings based on the positions of the switches. It is a satisfyingly light and crunchy feel.', 16, 1);
+insert into products (name, description, price, categorie) values ('GAN 13 Maglev', 'This cube has 88 magnets. 8 core magnets, 56 piece magnets, 12 repulsion magnets and 12 maglev rings all come together to give you the most amazing 3x3 ever concieved. ', 84, 2);
+insert into products (name, description, price, categorie) values ('X-MAN TORNADO V3 M Flagship', 'Is considered to be the best speedcube of 2022. It is very easy to customise. It as core magnets and maglev tech. The improved mechanism gives a more cohesive, smooth feel when turning.', 33, 2);
+insert into products (name, description, price, categorie) values ('MOYU RS3 M 2020', 'This cube is very famous for its extremely high value and cheap price. The cube has the classic MoYu dual adjustment system, factory installed magnets, and a vivid colour scheme.', 9, 2);
 insert into products (name, description, price, categorie) values ('QIYI MS', '', 10, 2);
 insert into products (name, description, price, categorie) values ('DAYAN TENGYUN M', '', 26, 2);
-insert into products (name, description, price, categorie) values ('YJ MGC', '', 20, 3);
-insert into products (name, description, price, categorie) values ('MOYU AOSU WR M', '', 39, 3));
-insert into products (name, description, price, categorie) values ('MFJS MEILONG', '', 8, 3));
-insert into products (name, description, price, categorie) values ('YJ ZHILONG MINI', '', 14, 3));
-insert into products (name, description, price, categorie) values ('YJ MGC', '', 23, 4);
+insert into products (name, description, price, categorie) values ('YJ MGC 4x4', '', 20, 3);
+insert into products (name, description, price, categorie) values ('MOYU AOSU WR M', '', 39, 3);
+insert into products (name, description, price, categorie) values ('MFJS MEILONG', '', 8, 3);
+insert into products (name, description, price, categorie) values ('YJ ZHILONG MINI', '', 14, 3);
+insert into products (name, description, price, categorie) values ('YJ MGC 5x5', '', 23, 4);
 insert into products (name, description, price, categorie) values ('MOYU AOCHUANG GST M', '', 40, 4);
 insert into products (name, description, price, categorie) values ('YJ ZHILONG MINI M', '', 17, 4);
-insert into products (name, description, price, categorie) values ('YG MGC', '', 26, 5);
+insert into products (name, description, price, categorie) values ('YG MGC 6x6', '', 26, 5);
 insert into products (name, description, price, categorie) values ('YUXIN LITTLE MAGIC', '', 14, 5);
 insert into products (name, description, price, categorie) values ('MFJS MEILONG', '', 14, 5);
-insert into products (name, description, price, categorie) values ('YJ MGC', '', 36, 6);
+insert into products (name, description, price, categorie) values ('YJ MGC 7x7', '', 36, 6);
 insert into products (name, description, price, categorie) values ('SHENSHOU TANK', '', 17, 6);
 insert into products (name, description, price, categorie) values ('YJ YUHU', '', 14, 7);
 insert into products (name, description, price, categorie) values ('GAN MEGAMINX', '', 58, 7);
@@ -81,7 +83,7 @@ insert into products (name, description, price, categorie) values ('YUXIN GIGAMI
 insert into products (name, description, price, categorie) values ('QIYI 2X2X3', '', 7, 12);
 insert into products (name, description, price, categorie) values ('QIYI GEAR 3X3', '', 8, 12);
 insert into products (name, description, price, categorie) values ('STACKMAT PRO TIMER GEN5', '', 33, 13);
-insert into products (name, description, price, categorie) values ('MAX COMMAND LUBE', '', 9, 13;
+insert into products (name, description, price, categorie) values ('MAX COMMAND LUBE', '', 9, 13);
 insert into products (name, description, price, categorie) values ('CUBICLE LABS MYSTIC LUBE', '', 5, 13);
 insert into products (name, description, price, categorie) values ('3X3 CALENDAR STICKERS', '', 4, 13);
 insert into products (name, description, price, categorie) values ('BLINDFOLD', '', 2,5, 13);
@@ -106,11 +108,11 @@ insert into categories (name) values ('Accessoires');
 insert into categories (name) values ('Merchandise');
 
 -- TABLE review
-insert into review (name, amount_stars, description) values ('1star');
-insert into review (name, amount_stars, description) values ('2star');
-insert into review (name, amount_stars, description) values ('3star');
-insert into review (name, amount_stars, description) values ('4star');
-insert into review (name, amount_stars, description) values ('5star');
+insert into review (name, amount_stars, description) values ('1star', '', '');
+insert into review (name, amount_stars, description) values ('2star', '', '');
+insert into review (name, amount_stars, description) values ('3star', '', '');
+insert into review (name, amount_stars, description) values ('4star', '', '');
+insert into review (name, amount_stars, description) values ('5star', '', '');
 
 -- TABLE company
 insert into company (name) values ('GAN');
